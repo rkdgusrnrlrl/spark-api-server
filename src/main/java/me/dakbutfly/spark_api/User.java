86bbe8a390bfbf9465a045ef8828d7c86edbb520
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-06-19 15:17:00 +0900

    fix: #2 사용자 등록 수정 API 테스트 추가 - 사용자 DataStructure 추가 - 사용자 리스트 API 테스트 추가 - 사용자 등록 API 테스트 추가

---
+++ src/main/java/me/dakbutfly/spark_api/User.java
@@ -0,0 +1,15 @@
+package me.dakbutfly.spark_api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
---
+++ src/test/java/me/dakbutfly/jmockit_example/api/TestUserApi.java
@@ -0,0 +1,56 @@
+package me.dakbutfly.jmockit_example.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.dakbutfly.spark_api.User;
+
+import me.dakbutfly.spark_api.Application;
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+import org.junit.*;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static spark.Spark.stop;
+
+public class TestUserApi {
+
+    @BeforeClass
+    public static void 서버_실행() {
+        Application.main(null);
+    }
+
+    @Test
+    public void 성공시_사용자정보를_출력() throws IOException {
+
+        Content content = Request.Get("http://localhost:4567/users")
+                                .execute()
+                                .returnContent();
+        String body = content.asString();
+        assertEquals(body, "{\"data\":{\"users\":[]}}");
+
+    }
+
+    @Test
+    public void 사용자정보를_등록_출력() throws IOException {
+        User user = User.builder()
+                .name("강현구")
+                .age(32)
+                .build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(user);
+
+        Content content = Request.Post("http://localhost:4567/users")
+                .bodyString(json, ContentType.APPLICATION_JSON)
+                .execute()
+                .returnContent();
+        String body = content.asString();
+        assertEquals(body, "{\"data\":{\"user\":"+json+"}}");
+    }
+
+    @AfterClass
+    public static void 서버_종료() {
+        stop();
+    }
+}
