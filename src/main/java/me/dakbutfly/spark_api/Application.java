d07a55e0dff1d3ed9e7dc08932d6ab8f9fc66ff9
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-06-19 17:07:25 +0900

    feat : #2 사용자 등록 후 리스트 테스트 추가 및 구현 - ArrayList 에 저장하는 방식으로 구현(분명 문제 있을 듯)

--- src/main/java/me/dakbutfly/spark_api/Application.java
+++ src/main/java/me/dakbutfly/spark_api/Application.java
@@ -3,20 +3,30 @@ package me.dakbutfly.spark_api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
+
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static me.dakbutfly.jmockit_example.common.ConvertJsonToInstance.*;
 
 public class Application {
+
+    private static ArrayList<User> userList = new ArrayList<>();
+
     public static void main(String[] args) {
 
         get("/users", (req, res) -> {
-            return "{\"data\":{\"users\":[]}}";
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(userList);
+
+            return "{\"data\":{\"users\":"+json+"}}";
         });
 
         post("/users", (req, res) -> {
             String body = req.body();
             User user = convertJsonStringTo(body, User.class);
+
+            userList.add(user);
 
             ObjectMapper objectMapper = new ObjectMapper();
             String json = objectMapper.writeValueAsString(user);
---
+++ src/test/java/me/dakbutfly/jmockit_example/api/TestUserApiBeforeSetUser.java
@@ -0,0 +1,59 @@
+package me.dakbutfly.jmockit_example.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.dakbutfly.spark_api.Application;
+import me.dakbutfly.spark_api.User;
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static spark.Spark.stop;
+
+public class TestUserApiBeforeSetUser {
+
+    @BeforeClass
+    public static void 서버_실행() throws IOException {
+
+        Application.main(null);
+        String json = getUserJsonString();
+
+        Content content = Request.Post("http://localhost:4567/users")
+                .bodyString(json, ContentType.APPLICATION_JSON)
+                .execute()
+                .returnContent();
+        String body = content.asString();
+        assertEquals(body, "{\"data\":{\"user\":"+json+"}}");
+    }
+
+    private static String getUserJsonString() throws JsonProcessingException {
+        User user = User.builder()
+                .name("강현구")
+                .age(32)
+                .build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(user);
+    }
+
+    @Test
+    public void 사용자_한명이_등록되_있어야함() throws IOException {
+
+        Content content = Request.Get("http://localhost:4567/users")
+                                .execute()
+                                .returnContent();
+        String body = content.asString();
+        assertEquals(body, "{\"data\":{\"users\":["+getUserJsonString()+"]}}");
+
+    }
+
+    @AfterClass
+    public static void 서버_종료() {
+        stop();
+    }
+}
